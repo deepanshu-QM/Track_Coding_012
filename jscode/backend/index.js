97f94624f27b26d9c67756d17517554f444741e2
deepanshu-QM <cdeepanshu231@gmail.com>
2025-11-25 03:27:10 +0530

    simple backend

---
+++ jscode/backend/index.js
@@ -0,0 +1,27 @@
+
+const express = require('express');
+const { use } = require('react');
+const app = express();
+
+function authMiddleware(req, res, next){
+    const username = req.headers.username;
+    const password = req.headers.password;
+
+    if(username != "prettier@34kl" || password != "pass@6780hh"){
+        res.status(400).json({  msg : "Incorrect Input"})
+    }else {
+        next();
+    }
+}
+
+function kidneyMiddleware(req, res, next){
+    const KidneyID = parseInt(req.query.KidneyID)
+    if(KidneyID != 1 &&  KidneyID != 2){
+        res.status(400).json({msg : "Incorrect Input "})
+    }else{
+        next();
+    }
+}
+app.get("/", authMiddleware, kidneyMiddleware, (req, res) => {
+    res.send("Kidney Is Fine ! .");
+})
