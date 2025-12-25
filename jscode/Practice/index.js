71a9b17da1b837e496e8c0057b9938c48001fe29
deepanshu-QM <cdeepanshu231@gmail.com>
2025-12-25 14:29:26 +0530

    Practice Commit

---
+++ jscode/Practice/index.js
@@ -0,0 +1,12 @@
+
+// Quick Revision Of all the Topics 
+//1) Automatically Type conversion(Java script accept almost any Program )
+
+console.log(4*null)
+console.log('-1' * 45)
+console.log("10" - 1)
+console.log("90" + 100)
+console.log("Ten" * 12)
+
+// 2) There only One value java Script that is not equal to itself that is NaN
+console.log(NaN ==  NaN)
