beeefca34290a1197badaa547ec8b20893e79603
deepanshu-QM <cdeepanshu231@gmail.com>
2025-12-26 18:09:36 +0530

    userLoginValidator function

--- jscode/Practice/index.js
+++ jscode/Practice/index.js
@@ -1,23 +1,25 @@
 
-/* Quick Revision Of all the Topics 
-//1) Automatically Type conversion(Java script accept almost any Program )
 
-console.log(4*null)
-console.log('-1' * 45)
-console.log("10" - 1)
-console.log("90" + 100)
-console.log("Ten" * 12)
+//Clean Code : UserLoginValidator 
+function userLoginValidator(username,password){
+    try{
+        if(!username || typeof username !== "string"){
+            throw new Error("Your username is Wrong : use string Type ")
+        }
+        if(typeof password !== "string"){
+            throw new Error("Your password is Wrong : use string Type")
+        }
+        if(password.length <= 6){
+            throw new Error("Your password must be more than 6 character ")
+        }
+        if(username.toLowerCase() !== "john snow"){
+            throw new Error("Invalid username")
+        }
 
-// 2) There only One value java Script that is not equal to itself that is NaN
-console.log(NaN ==  NaN)
+        console.log("Login sucessfully")
+    }catch(error){
+        console.log("Error : ", error.message)
+    }
+}
 
-
-//3 
-console.log("YouAreTheSamePerson" > "PriceMrWales")
-console.log("YouAreTheSamePerson" ==  "PriceMrWales")
-console.log(0 ?? 100)
-console.log(null ?? 100)
-
-console.log(`null == undefined  :${null == undefined}`)
-console.log(`null == 0 : ${null == 0}`)  */
-
+userLoginValidator("Gabriell Daisey", "uytgvjgvbbmhg")
