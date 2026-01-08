af6709dd2f74579872580397fb31be5f59017343
deepanshu-QM <cdeepanshu231@gmail.com>
2026-01-08 18:06:28 +0530

    Logger Component with createWriteStream

---
+++ jscode/fileHandling/loggerComponent(WriteStream)/logger.js
@@ -0,0 +1,45 @@
+
+
+import fs from "fs";
+import path from "path";
+
+let LogsStream;
+let canWrite = true;
+
+function createLogStream(){
+    //Logger format it 
+    const date = new date().toISOString().split(0,10);
+    const LogFilePath = path.join(`Logs`, `${date}.log`)
+
+    //logger --> stream (stream allocates internal Buffer 64kb )
+    return fs.createWriteStream(LogFilePath, {
+        flags : "a",
+        highWaterMark : 64*1024
+    });
+}
+export function initlogger(){
+    const LogsStream = createLogStream();
+
+    LogsStream.on(`drain`,()=> {
+        console.log(`Buffer is Drained , Resume Logs`);
+        canWrite = true;
+    });
+}
+
+//Rotate the log
+
+setInterval(() => {
+    const today = new Date().toISOString().split(0,10)
+    //If logs are not Today
+    if(!LogsStream.path.includes(today)){
+        //close the logs 
+        LogsStream.end(() => {
+            //start new Logs (File Open)
+            LogsStream = createLogStream();
+            canWrite = true;
+        });
+    }
+},60*1000)
+
+/* Flow Of the Program  
+app send message ----> logger -----> logger format it(add Time stamps , format message) ---->  logger Give to stream(It act Like pipline b\w app and disk , queue) ----->Stream put it into buffer(temporary memory where logs are stored)  -----> Disk write from buffer slowly*/
