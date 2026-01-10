44b4037f57ff737ff4de2ecadb1099fd462a1d99
deepanshu-QM <cdeepanshu231@gmail.com>
2026-01-10 17:49:15 +0530

    Completing Server LOgic

--- jscode/backend/2PracticeServer.js
+++ jscode/backend/2PracticeServer.js
@@ -1,5 +1,4 @@
 
-
 const express = require("express");
 const app = express();
 const port = 3000;
@@ -57,6 +56,37 @@ app.put("/p", (req,res) => {
         message : "Salary Record added Sucessfully",
         updatedData : Employees[0].Salary
     });
+})
+
+app.delete("/d", (req, res) => {
+    if(FnoOfTimesSalaryNotReceived()){
+        let newSalary = [];
+        for(i=0;i<Employees[0].Salary.length;i++){
+            if(Employees[0].Salary[i].salaryReceived){
+                newSalary.push({
+                    salaryReceived : true
+                })
+            }
+        }
+        Employees[0].Salary = newSalary;
+        res.json({
+            Msg : "Unpaid  Salaries Removed SuccessFully "
+        });
+    }else{
+        res.status(411).json({
+            msg : "No paid Salary Found."
+        });
+    }
+    //Buisness Logic
+    function FnoOfTimesSalaryNotReceived(){
+        let atnoOfTimesSalaryNotReceived = false;
+        for (let i=0;i<Employees[0].Salary.length;i++){
+            if(!Employees[0].Salary[i].salaryReceived){
+                atnoOfTimesSalaryNotReceived = true;
+            }
+        }
+        return atnoOfTimesSalaryNotReceived;
+    }
 })
 
 app.listen(port ,() => {
