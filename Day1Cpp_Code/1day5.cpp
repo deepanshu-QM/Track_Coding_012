cf90d923f28e95ca5ad6336b20bd572ce9139773
deepanshu-QM <cdeepanshu231@gmail.com>
2026-01-12 18:09:50 +0530

    File structure Change and pattern print statements

---
+++ Day1Cpp_Code/1day5.cpp
@@ -0,0 +1,26 @@
+
+/* Question 1 : Square Pattern Printing : n = 4  
+1 2 3 4
+1 2 3 4
+1 2 3 4
+1 2 3 4*/
+
+#include<iostream>
+using namespace std;
+
+int squarePattern(int n){
+    for(int i=1;i<=n;i++){  //Outer Loop
+        for(int j=1;j<=n;j++){
+            cout<<j;
+        }
+        cout<<endl;
+    }
+}
+int main(){
+    int n;
+    cout<<"Enter n : "<<endl;
+    cin>>n;
+
+    squarePattern(n);
+    return 0;
+}
