32332b13535b472f67acc64c8c1dc801c9ec6fbc
deepanshu-QM <cdeepanshu231@gmail.com>
2026-01-23 22:46:41 +0530

    Loop

---
+++ Day4Cpp_Code/main.cpp
@@ -0,0 +1,15 @@
+//Print 1 to N 
+#include<iostream>
+using namespace std;
+
+int main(){
+    int N,i;
+    cout<<"Enter N : "<<endl;
+    cin>>N;
+    
+    i=1;
+    while(i<=N){
+        cout<<i<<endl;
+        i++;
+    }
+}
---
+++ Day4Cpp_Code/main2.cpp
@@ -0,0 +1,16 @@
+
+//print From N to 1
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n,i;
+    cout<<"Enter n : "<<endl;
+    cin>>n;
+
+    i=n;
+    while(i >= 1){
+        cout<<i<<endl;
+        i++;
+    }
+}
