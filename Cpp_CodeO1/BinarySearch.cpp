3602afd72218aaced9485d17354ba2c144ccb523
deepanshu-QM <cdeepanshu231@gmail.com>
2026-02-20 22:25:32 +0530

    Binary Search

---
+++ Cpp_CodeO1/BinarySearch.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int binarySearch(int arr[], int n, int target) {
+    int left = 0;
+    int right = n - 1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        if (arr[mid] == target)
+            return mid;   // Element found
+
+        else if (arr[mid] < target)
+            left = mid + 1;  // Search right half
+
+        else
+            right = mid - 1; // Search left half
+    }
+
+    return -1;  // Element not found
+}
+
+int main() {
+    int arr[] = {1, 3, 5, 7, 9, 11};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int target = 7;
+
+    int result = binarySearch(arr, n, target);
+
+    if (result != -1)
+        cout << "Element found at index: " << result;
+    else
+        cout << "Element not found";
+
+    return 0;
+}
